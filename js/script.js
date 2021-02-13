e73ff3b0c91b1ec5628be5228e64a575db2a0790
Dmitriy <d.a16@yandex.ru>
2021-02-13 10:58:49 +0300

    connect css

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const obj = {
+	a: 5,
+	b: 1
+};
+
+//const copy = obj; //Ссылка на уже существующий объект
+
+//copy.a = 10;
+
+//console.log(copy);
+//console.log(obj);
+
+function copy(mainObj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+
+	return objCopy;
+}
+
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
